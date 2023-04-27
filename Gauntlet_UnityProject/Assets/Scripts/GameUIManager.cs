07100fd20d010bfcd9e1b708e8036859f10908e4
Kurisaron <103210710+Kurisaron@users.noreply.github.com>
2023-04-27 13:47:13 -0700

    class selection prog

--- Gauntlet_UnityProject/Assets/Scripts/GameUIManager.cs
+++ Gauntlet_UnityProject/Assets/Scripts/GameUIManager.cs
@@ -16,7 +16,35 @@ public class GameUIManager : Singleton<GameUIManager>
     [SerializeField]
     private Text levelUIText;
 
+    [SerializeField]
+    private GameObject classChoices;
+
     // FUNCTIONS
+    private void Update()
+    {
+        ShowClassChoices();
+
+    }
+
+    private void UpdateHealth()
+    {
+        for (int i = 0; i < playerContainers.Length; i++)
+        {
+            if (!playerContainers[i].frame.activeInHierarchy) continue;
+
+        }
+    }
+
+    private void UpdateScore()
+    {
+        for (int i = 0; i < playerContainers.Length; i++)
+        {
+            if (!playerContainers[i].frame.activeInHierarchy) continue;
+
+            playerContainers[i].ScoreText.text = GameManager.Instance.players[i].score.ToString();
+        }
+    }
+
     public void DisableAllContainers()
     {
         for(int i = 0; i < playerContainers.Length; i++)
@@ -25,11 +53,39 @@ public class GameUIManager : Singleton<GameUIManager>
         }
     }
 
-    public void EnableContainer(int index) => playerContainers[index].frame.SetActive(true);
+    public void EnableContainer(int index)
+    {
+        PlayerStats stats = playerContainers[index];
+        stats.frame.SetActive(true);
+        stats.ClassName.text = "Choose Class";
+        stats.ClassName.fontSize = 13;
+        stats.ScoreText.text = GameManager.Instance.players[index].score.ToString();
+    }
 
     public void DisableContainer(int index)
     {
         if (playerContainers[index] != null) playerContainers[index].frame.SetActive(false);
+    }
+
+    public void SetClass(int index, ClassEnum classEnum)
+    {
+        PlayerStats stats = playerContainers[index];
+        stats.ClassName.text = classEnum.ToString();
+        stats.ClassName.fontSize = 18;
+
+    }
+
+    private void ShowClassChoices()
+    {
+        bool allCharactersHaveClass = true;
+        for (int i = 0; i < playerContainers.Length; i++)
+        {
+            if (!playerContainers[i].frame.activeInHierarchy) continue;
+
+            if (!GameManager.Instance.players[i].gameObject.GetComponent<PlayerInputEvents>().ClassSelected) allCharactersHaveClass = false;
+        }
+
+        if (allCharactersHaveClass == classChoices.activeInHierarchy) classChoices.SetActive(!allCharactersHaveClass);
     }
 
     // CLASSES
@@ -47,5 +103,20 @@ public class GameUIManager : Singleton<GameUIManager>
         private Image[] upgradeIndicators;
         [SerializeField]
         private Image[] inventoryIndicators;
+
+        public Text ClassName
+        {
+            get { return className; }
+        }
+
+        public Text ScoreText
+        {
+            get { return scoreText; }
+        }
+
+        public Text HealthText
+        {
+            get { return healthText; }
+        }
     }
 }
