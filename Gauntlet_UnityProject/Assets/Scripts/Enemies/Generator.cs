26e177877e4e2200454eae43b1044b13392fddd6
extremeLworld <achen@mail.csuchico.edu>
2023-05-03 23:37:32 -0700

    adding a loop to the generator object pooling
    it doesn't actually work yet

--- Gauntlet_UnityProject/Assets/Scripts/Enemies/Generator.cs
+++ Gauntlet_UnityProject/Assets/Scripts/Enemies/Generator.cs
@@ -4,32 +4,29 @@ using UnityEngine;
 
 public class Generator : Foe
 {
+    private List<EnemyType> enemyObjectList = new List<EnemyType>();
 
-    private void OnEnable()
+    private void Start()
     {
+        enemyObjectList.Add(enemyType);
         StartCoroutine(SpawnEnemy(enemyType));
     }
 
 
     private IEnumerator SpawnEnemy(EnemyType enemyType)
     {
-        GameObject enemyObject = ObjectPooler.Instance.GetPooledObject(enemyType.ToString());
-        //List<GameObject> enemyObjectList = 
-
         while (true)
         {
-            /*foreach (var item in collection)
+            foreach (EnemyType enemy in enemyObjectList)
             {
-
-            }*/
-            
-            
-            
-            if (enemyObject != null)
-            {
-                enemyObject.transform.position = transform.position;
-                enemyObject.transform.rotation = transform.rotation;
-                enemyObject.SetActive(true);
+                GameObject enemyToSpawn = ObjectPooler.Instance.GetPooledObject(enemyType.ToString());
+                
+                if (enemyToSpawn != null)
+                {
+                    enemyToSpawn.transform.position = transform.position;
+                    enemyToSpawn.transform.rotation = transform.rotation;
+                    enemyToSpawn.SetActive(true);
+                }
             }
 
             yield return new WaitForSeconds(1);
